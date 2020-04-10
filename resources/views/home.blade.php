fc515d7ba46c5a0791d7f38f858a2dfd0bd6198e
Zee0a <61969051+Zee0a@users.noreply.github.com>
2020-04-10 17:19:47 +0100

    added username functionality to login

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,25 @@
                 <div class="pr-5"><strong>55k</strong> followers </div>
                 <div class="pr-5"><strong>173</strong> following </div>
             </div>
+            <div class="pt-4 font-weight-bold">TU Dublin</div>
+            <div>Instagram account For Technological University Dublin. Share your pics and videos with us! #TUDublin #WeAreTUDublin</div>
+            <div><a href ="#">https://tudublin.ie/</a></div>
         </div>
     </div>
+
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://pbs.twimg.com/profile_images/1186964392161755138/oufVUcql_400x400.jpg" class="w-100">
+        </div>
+        <div class="col-4">
+             <img src="https://pbs.twimg.com/profile_images/1186964392161755138/oufVUcql_400x400.jpg" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://pbs.twimg.com/profile_images/1186964392161755138/oufVUcql_400x400.jpg" class="w-100">
+        </div>
+
+    </div>
+
+
 </div>
 @endsection
