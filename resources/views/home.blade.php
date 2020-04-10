1f41038e2010f58a28a55f570094cdcf4fcf95c4
Zee0a <61969051+Zee0a@users.noreply.github.com>
2020-04-10 15:09:44 +0100

    added new image, divs, font style & made changes to my scss files

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,20 +2,17 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://pbs.twimg.com/profile_images/1076024757580115969/zbfizpde_400x400.jpg" class="rounded-circle" height="150px">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>TuDublin</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>101</strong> posts</div>
+                <div class="pr-5"><strong>55k</strong> followers </div>
+                <div class="pr-5"><strong>173</strong> following </div>
             </div>
         </div>
     </div>
